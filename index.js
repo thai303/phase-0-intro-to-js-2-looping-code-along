31ef20b6f16398a731f89ea3bd16c7459ad1a203
Thai Xiong <thai303@hotmail.com>
2022-05-18 16:10:12 -0600

    commit tree 36fc73b45c76b10e2e03d73086cf9aecd1c602df

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(name, event){
+    let thankYouCards = []
+    for (let i = 0; i < name.length; i++ ) {
+        thankYouCards.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`)
+    }
+    return thankYouCards
+}
+
+function countDown(n) {
+    console.log(n)
+    return n ? countDown(n - 1) : 0;
+}
+
+countDown(10)
